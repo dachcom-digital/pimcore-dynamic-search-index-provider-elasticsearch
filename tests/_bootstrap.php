eac2c8a4818e5b0382f571a41a35e0e872ba34b0
Stefan Hagspiel <shagspiel@dachcom.ch>
2020-12-07 09:21:40 +0100

    [Tests] Github Actions

---
+++ tests/_bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+
+use Pimcore\Bootstrap;
+use DachcomBundle\Test\Util\Autoloader;
+
+include __DIR__ . '/../vendor/autoload.php';
+
+define('PIMCORE_KERNEL_CLASS', '\DachcomBundle\Test\App\TestAppKernel');
+define('PIMCORE_TEST', true);
+
+Bootstrap::setProjectRoot();
+Bootstrap::bootstrap();
+
+/**
+ * @var $loader \Composer\Autoload\ClassLoader
+ */
+Autoloader::addNamespace('Pimcore\Tests', PIMCORE_PROJECT_ROOT . '/vendor/pimcore/pimcore/tests/_support');
+Autoloader::addNamespace('Pimcore\Model\DataObject', __DIR__ . '/_output/var/classes/DataObject');
+
+if (!defined('TESTS_PATH')) {
+    define('TESTS_PATH', __DIR__);
+}
