d9456f236797f169b8ff740ccd66764894474817
Stefan Hagspiel <shagspiel@dachcom.ch>
2020-12-16 09:56:24 +0100

    remove  symfony 4.3 deprecations

--- src/DsElasticSearchBundle/DependencyInjection/Configuration.php
+++ src/DsElasticSearchBundle/DependencyInjection/Configuration.php
@@ -9,9 +9,6 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('ds_elasticsearch');
-
-        return $treeBuilder;
+        return new TreeBuilder('ds_elasticsearch');
     }
 }
